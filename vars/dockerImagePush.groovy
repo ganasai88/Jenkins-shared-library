d73a3634e4ec5a34681ccad968033156bd5c8268
ganasai88 <ganasai88@gmail.com>
2023-04-23 18:53:56 +0530

    1

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,6 +1,6 @@
 def call(String project, String ImageTag, String hubUser){
      withCredentials([usernamePassword(
-             credentialsId: "docker",
+             credentialsId: "hubUser",
              usernameVariable: "USER",
              passwordVariable: "PASS"
      )]) {
